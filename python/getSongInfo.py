84b3b79b381f501e6ec677e911baeb3a385d16dd
Frieder Haizmann <urpyg@student.kit.edu>
2024-04-28 17:47:30 +0200

    Attempts at using new Spotify Auth Flow and getting podcast info

--- python/getSongInfo.py
+++ python/getSongInfo.py
@@ -5,14 +5,19 @@ import spotipy.util as util
 import requests
 from io import BytesIO
 from PIL import Image
+from spotipy.oauth2 import SpotifyOAuth
+
 
 def getSongInfo(username, token_path):
   scope = 'user-read-currently-playing'
+  # auth_manager=SpotifyOAuth(scope=scope, open_browser=False)
+  # token = auth_manager.get_access_token()
+  # sp = spotipy.Spotify(auth_manager=auth_manager)
   token = util.prompt_for_user_token(username, scope, cache_path=token_path)
 
   if token:
       sp = spotipy.Spotify(auth=token)
-      result = sp.current_user_playing_track()
+      result = sp.current_user_playing_track()# sp.current_playback(additional_types="episode")
     
       if result is None:
          #print("No song playing")
